c01d9b70dd9f1df04f603e6b96f959cfaf00b832
rhysgoehring <rhysgoehring@gmail.com>
2018-05-10 10:47:36 -0600

    webpack boilerplate complete, webpack config'd, rendering example react component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './app.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+      </div>
+    );
+  }
+}
+export default App;
